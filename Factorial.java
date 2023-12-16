a29e674a8e64f8d3d1227e7d232705e22238aba7
shubham317ht <shubham317.ht@gmail.com>
2023-12-16 18:02:41 +0530

    task: user input feature added in factorial

--- Factorial.java
+++ Factorial.java
@@ -1,10 +1,16 @@
 package test_app;
 
+import java.util.Scanner;
+
 public class Factorial {
 
     public static void main(String[] args) {
         
-        System.out.println(factorial(5));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no. to find factorial :");
+        int input = sc.nextInt();
+        int fact  = factorial(input);
+        System.out.println("The factorial of "+input+" is "+fact);
     }
 
     private static int factorial(int i) {
